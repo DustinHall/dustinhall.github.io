f8dad0db3875242c4091ef27250518435095bdac
Dustin Hall <Dustnhall@gmail.com>
2021-06-30 13:53:17 -0400

    validation doesnt work

--- aircraft-system-overviews/script2.js
+++ aircraft-system-overviews/script2.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button');
 
 button.onclick = function() {
   let name = prompt('What is your name?');
-  if (name = '') {
+  if (name = 'blank') {
     alert('Name must be filled out')
   } else {
   alert('Hello ' + name + ', nice to see you!');
