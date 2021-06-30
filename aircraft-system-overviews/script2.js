de88fa28750aa67cfe073890e0271c2cee928aa2
Dustin Hall <Dustnhall@gmail.com>
2021-06-30 18:42:26 -0400

    nope

--- aircraft-system-overviews/script2.js
+++ aircraft-system-overviews/script2.js
@@ -3,9 +3,7 @@ const button = document.querySelector('button');
 button.onclick = function() {
   let name = prompt('What is your name?');
   while (name == '') {
-    alert('Must have a name');
-    button.onclick = function ();
-    return ;  
+    alert('Must have a name');  
   } else {
   alert('Hello ' + name + ', nice to see you!');
 }
