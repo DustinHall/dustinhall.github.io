9faee36221388298b896f28f8700851ff63a0d1a
Dustin Hall <Dustnhall@gmail.com>
2021-06-30 13:39:21 -0400

    validation box

--- aircraft-system-overviews/script2.js
+++ aircraft-system-overviews/script2.js
@@ -4,3 +4,11 @@ button.onclick = function() {
   let name = prompt('What is your name?');
   alert('Hello ' + name + ', nice to see you!');
 }
+
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
