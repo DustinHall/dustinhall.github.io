d9208d15e7761b5d54a70b841476c119c824c98d
Dustin Hall <Dustnhall@gmail.com>
2021-06-30 20:27:22 -0400

    probably not

--- aircraft-system-overviews/script2.js
+++ aircraft-system-overviews/script2.js
@@ -1,10 +1,11 @@
 const button = document.querySelector('button');
+const autoClick = document.querySelector('button') 
 
 button.onclick = function() {
   let name = prompt('What is your name?');
   while (name == '') {
     alert('Must have a name');
-    function autoClick () {'button'};  
+    function autoClick ();  
   } else {
   alert('Hello ' + name + ', nice to see you!');
   }
