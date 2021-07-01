f6bd9912c449d0dc9a242e13422aa34ee10c0f24
Dustin Hall <Dustnhall@gmail.com>
2021-06-30 20:16:45 -0400

    trying autoClick

--- aircraft-system-overviews/script2.js
+++ aircraft-system-overviews/script2.js
@@ -4,8 +4,7 @@ button.onclick = function() {
   let name = prompt('What is your name?');
   while (name == '') {
     alert('Must have a name');
-    function();
-    return;  
+    function autoClick () {'button'};  
   } else {
   alert('Hello ' + name + ', nice to see you!');
   }
