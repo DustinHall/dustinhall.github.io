fcbd5571a3244529d7b9312d6bd037c7fd55785b
Dustin Hall <Dustnhall@gmail.com>
2021-06-30 20:04:04 -0400

    try this time

--- aircraft-system-overviews/script2.js
+++ aircraft-system-overviews/script2.js
@@ -2,8 +2,10 @@ const button = document.querySelector('button');
 
 button.onclick = function() {
   let name = prompt('What is your name?');
-  if (name == '') {
-    alert('Must have a name');  
+  while (name == '') {
+    alert('Must have a name');
+    function();
+    return;  
   } else {
   alert('Hello ' + name + ', nice to see you!');
   }
